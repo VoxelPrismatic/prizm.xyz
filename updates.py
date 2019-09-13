2197b76fcc959e878f2a1fea0cfd53e43f619f78
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-12 20:47:30 -0500

    Update updates.py

--- updates.py
+++ updates.py
@@ -1,1 +1,97 @@
+from browser import document
+changes = {"09-12-2019 [BIG]":"""\
+> Added a public exec command
+> > Fixed a bug where newlines wouldn't actually go through
+> > I heard about some issues with encoding but I can't verify that rn
+> Fixed the aliases of the help command [again]
+> Fixed very minor bugs with the simplify command
+> Updated the graph command
+> > The graph command now supports both-axis graphing [x^2+y^2=4, etc]
+> > It should now actually graph faster when using more equations
+> > Uses a new pallette that overall looks nicer
+> > Fixed a couple bugs
+> > Now is higher resolution
+> > Now has X and Y axes
+> > Now won't break when the vars are uppercase
+> > Should actually parse faster and break less when doing so""",
+           
+           "09-09-2019":"""\
+> Added the simplify command
+> Added more features to that command
+> Fixed bugs with the help command
+> > All aliases after the first one would have a '.' in front
+> Added the calc command
+> > Added more functions
+> ] Why did it take so long? I was trying to prevent injection, and I'm fairly certain I succeeded""",
+           
+           "08-26-2019":"""\
+> Cleaned up files and code to make it more readable
+> Added help text for the clrin command
+> Added the clrto command, it clears all messages to a given message ID
+> > Useful if you don't know the exact amount of kessages to clear
+> Updated the graph command so you can now graph on the y axis too [x=y^2, y=x^2]
+> > Support for xy functions [x^2+y^2=4] has not been added yet, I'm working on it tho""",
+           
+           "08-22-2019 [MASSIVE]":"""\
+> Added an actually decent AI [ ;]text hello ]
+> Fixed a bug where "no" would register as a bool and break it 
+> Fixed a bug where smart quotes [‘’ “”] would register as a bool break it
+> Fixed a bug where any colons would register as a dict and break it
+> Now responds before it re-analyses
+> New logic will come soon, it currently only chooses the best response
+> Removed old files
+> Updated GitHub
+> Removed old AIs because they were trash
+> Updated main file to not be trash
+> Cleaned up so much code it's not even funny
+> Added docs to other things
+> Added a learning command
+> Updated more files
+> Updated the inv command to be dynamic
+> Working on a better audit command
+> Fixed bugs and things""",
+           
+           "08-20-2019":"""\
+> Added the reddit command
+> Fixed some bugs with that
+> Added the captcha command
+> Added the 8ball command""",
+           
+           "08-19-2019":"""\
+> Fixed the 2048 command
+> > Now groups properly when not moving right
+> > Now doesn't break when there are multiple instances
+> > Doesn't edit twice
+> > Now ends the game properly, whereas before it would end if it couldn't add any new tiles""",
+           
+           "08-18-2019":"""\
+> Added 2048
+> Fixed bugs with the 2048 command
+> Added 2048 to the help command
+> Updated the help command to change the prefix in the page
+> Fixed bugs with the above
+> Updated the ;]inv command to comply with new permission requirements
+> Added this site link to the inv command""",
+           
+           "08-13-2019":"""\
+> Updated the help command to be automatic documentation
+> Updated the help command so you can now do ';]help {command name}'
+> Added new interactive commands [cuddle, kiss, throw]
+> Added music capabilities"""}
 
+changelog = document.getElementById("changes")
+for key in changes:
+    date = document.createElement("DIV")
+    text = document.createTextNode(key)
+    date.class = "header"
+    date.append(text)
+    changelog.append(date)
+    
+    changed = document.createElement("DIV")
+    text = document.createTextNode("")
+    changed.class = "content consect"
+    changed.append(text)
+    changed.innerHTML = changes[key].replace('\n','<br>')
+    changelog.append(changed)
+    
+    
