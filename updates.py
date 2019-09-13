22236ced62bd176663b7c4aab232f63fbfa09e5a
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-12 21:12:01 -0500

    Update updates.py

--- updates.py
+++ updates.py
@@ -6,14 +6,15 @@ changes = {"09-12-2019 [BIG]":"""\
 > Fixed the aliases of the help command [again]
 > Fixed very minor bugs with the simplify command
 > Updated the graph command
-> > The graph command now supports both-axis graphing [x^2+y^2=4, etc]
+> > The graph command now supports both-axis graphing [<span class="mono">x^2+y^2=4</span>]
 > > It should now actually graph faster when using more equations
 > > Uses a new pallette that overall looks nicer
 > > Fixed a couple bugs
 > > Now is higher resolution
-> > Now has X and Y axes
+> > Now has X and Y axes clearly marked
 > > Now won't break when the vars are uppercase
-> > Should actually parse faster and break less when doing so""",
+> > Should actually parse faster and break less when doing so
+> > Shouldn't break with <span class="mono">x=...</span> or <span class="mono">y=...</span>""",
            
            "09-09-2019":"""\
 > Added the simplify command
@@ -29,13 +30,14 @@ changes = {"09-12-2019 [BIG]":"""\
 > Added help text for the clrin command
 > Added the clrto command, it clears all messages to a given message ID
 > > Useful if you don't know the exact amount of kessages to clear
-> Updated the graph command so you can now graph on the y axis too [x=y^2, y=x^2]
-> > Support for xy functions [x^2+y^2=4] has not been added yet, I'm working on it tho""",
+> Updated the graph command so you can now graph on the y axis too 
+> > [<span class="mono">x=y^2</span> and <span class="mono">y=x^2</span> are supported]
+> > Support for xy functions [<span class="mono">x^2+y^2=4</span>] has not been added yet, I'm working on it tho""",
            
            "08-22-2019 [MASSIVE]":"""\
-> Added an actually decent AI [ ;]text hello ]
+> Added an actually decent AI [<span class="mono">;]text hello</span>]
 > Fixed a bug where "no" would register as a bool and break it 
-> Fixed a bug where smart quotes [‘’ “”] would register as a bool break it
+> Fixed a bug where smart quotes [<span class="mono">‘’“”</span>] would register as a bool break it
 > Fixed a bug where any colons would register as a dict and break it
 > Now responds before it re-analyses
 > New logic will come soon, it currently only chooses the best response
