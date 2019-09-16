e5ad609398bc108677bedf0108381eb7f524ecf9
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-16 10:02:15 -0500

    Update about.py

--- about.py
+++ about.py
@@ -1,1 +1,22 @@
 from browser import document
+changes = {""}
+
+changelog = document.getElementById("about")
+for key in changes:
+    date = document.createElement("DIV")
+    text = document.createTextNode(key)
+    date.class_name = "header"
+    date.append(text)
+    changelog.appendChild(date)
+    
+    changed = document.createElement("DIV")
+    text = document.createTextNode("")
+    changed.class_name = "content consect"
+    changed.appendChild(text)
+    replace = {'\n':'<br>',
+               '<code>':'<span class="mono dark">',
+               '</code>':'</span>'}
+    for re in replace:
+        changes[key] = changes[key].replace(re, replace[re])
+    changed.innerHTML = changes[key]
+    changelog.appendChild(changed)
