68002c7e621da47311d2d351b159b1b6f7686ad0
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 08:12:30 -0500

    Update faqs.py

--- loader/faqs.py
+++ loader/faqs.py
@@ -1,26 +1,22 @@
 from browser import document
 
-faq = {"OwO WHAT IS THIS?": "This is an empty FAQ because nobody asked any questions yet UwU"}
+content = {"OwO WHAT IS THIS?": "This is an empty FAQ because nobody asked any questions yet UwU"}
 
-def addSect(content, element):
-    for key in content:
-        title = document.createElement("DIV")
-        text = document.createTextNode(key)
-        title.class_name = "header"
-        title.append(text)
-        element.appendChild(title)
-    
-        stuff = document.createElement("DIV")
-        text = document.createTextNode("")
-        stuff.class_name = "content consect"
-        stuff.appendChild(text)
-        replace = {'\n':'<br>',
-                   '<code>':'<span class="mono dark">',
-                   '</code>':'</span>'}
-        for re in replace:
-            content[key] = content[key].replace(re, replace[re])
-        stuff.innerHTML = changes[key]
-        element.appendChild(stuff)
-        #return title, stuff
-faqs = document.getElementById("faq")
-addSect(faq, faqs)
+element = document.getElementById("faq")
+for key in content:
+    title = document.createElement("DIV")
+    text = document.createTextNode(key)
+    title.class_name = "header"
+    title.append(text)
+    element.appendChild(title)
+    stuff = document.createElement("DIV")
+    text = document.createTextNode("")
+    stuff.class_name = "content consect"
+    stuff.appendChild(text)
+    replace = {'\n':'<br>',
+               '<code>':'<span class="mono dark">',
+               '</code>':'</span>'}
+    for re in replace:
+        content[key] = content[key].replace(re, replace[re])
+    stuff.innerHTML = changes[key]
+    element.appendChild(stuff)
