fcb4a4a33bce70c82d6acf7fc8f32fe872799a6d
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 11:06:48 -0500

    Update script.js

--- loader/script.js
+++ loader/script.js
@@ -1,16 +1,6 @@
 var tap = document.getElementsByClassName("clicky");
 for (var i = 0; i < tap.length; i++) {
     tap[i].addEventListener("click", function() {
-        var clicky = document.getElementsByClassName("clicky");
-        for (var j = 0; j < clicky.length; j++) {
-            if (clicky[j].id != this.id) {
-                clicky[j].classList.toggle("active", clicky[j].style.height != null);
-                var ct2 = clicky[j].nextElementSibling;
-                if (ct2.style.height){
-                    ct2.style.height = null;
-                }
-            }
-        }
         this.classList.toggle("active")
         var content = this.nextElementSibling;
         if (content.style.height){
