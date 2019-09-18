71fc9533a2065efe2df80ecd89f4bd303d40a15e
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-18 08:33:34 -0500

    Update faqs.py

--- loader/faqs.py
+++ loader/faqs.py
@@ -3,20 +3,20 @@ from browser import document
 content = {"OwO WHAT IS THIS?": "This is an empty FAQ because nobody asked any questions yet UwU"}
 
 element = document.getElementById("faq")
+rep = {'\n':'<br>',
+       '<code>':'<span class="mono dark">',
+       '</code>':'</span>'}
 for key in content:
     title = document.createElement("DIV")
     text = document.createTextNode(key)
     title.class_name = "header"
     title.append(text)
-    element.appendChild(title)
+    element.append(title)
     stuff = document.createElement("DIV")
-    text = document.createTextNode("")
+    text = document.createTextNode('~loading~')
     stuff.class_name = "content consect"
-    stuff.appendChild(text)
-    replace = {'\n':'<br>',
-               '<code>':'<span class="mono dark">',
-               '</code>':'</span>'}
-    for re in replace:
-        content[key] = content[key].replace(re, replace[re])
-    stuff.innerHTML = changes[key]
-    element.appendChild(stuff)
+    stuff.append(text)
+    for re in rep:
+        content[key] = content[key].replace(re,rep[re])
+    stuff.innerHTML = content[key]
+    element.append(stuff)
