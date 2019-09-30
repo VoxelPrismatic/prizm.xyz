8e751300df1278e648899b320051a6d1e0be4ff2
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-09-30 17:22:28 -0500

    Update script.js

--- loader/script.js
+++ loader/script.js
@@ -3,11 +3,11 @@ for (var i = 0; i < tap.length; i++) {
     tap[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
+        content.style.transition = `${(content.scrollHeight + 5)/1000}s`
         if (content.style.height){
             content.style.height = null;
         } else {
             content.style.height = `${content.scrollHeight + 5}px`;
-            content.style.transition = `height ${(content.scrollHeight + 5)/1000}s`
         }
     });
 }
