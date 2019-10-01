d4f0a351345e1a65c43ce8da952899c576190c30
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-01 12:54:32 -0500

    Update script.js

--- loader/script.js
+++ loader/script.js
@@ -1,9 +1,8 @@
 var tap = document.getElementsByClassName("clicky");
 for (var i = 0; i < tap.length; i++) {
-    tap[i].nextElementSibling.style.transition = `height ease ${(content.scrollHeight + 5)/1000}s`;
     tap[i].onclick = function() {
         if (this.classList.toggle("active")) {
-            this.nextElementSibling.style.height = `${content.scrollHeight + 5}px`
+            this.nextElementSibling.style.height = `${this.nextElementSibling.scrollHeight + 5}px`
         } else {
             this.nextElementSibling.style.height = "0px";
         }
