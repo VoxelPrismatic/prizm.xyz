294b0570f4bfee7d58b21d98cb5ec9e0dedfb463
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-04 09:34:00 -0500

    Update aboutme.py

--- loader/aboutme.py
+++ loader/aboutme.py
@@ -14,12 +14,12 @@ It actually runs pretty well for 8GB of RAM, 2GB of SWAP [Virtual Ram], and a 2.
 I made all of these by myself, fyi 
 Too find many cool faces that the bot uses, please visit this <a href="https://discord.gg/NHqbnyc">link</a> ;]
 If you have Discord Nitro and want to use the sick emoji this bot comes with, please visit the \
-<a href="https://discord.gg/eYMyfcd">emoji server ;]</a>""",
+>$https://discord.gg/eYMyfcd$$emoji server ;]$<""",
           "WTF IS THAT LOGO?":"""\
 That logo is something that I made quite a while ago, except that version was much worse. \
 This is iteration 3, with shiny colors and more glow. It should be a prism of light, \
 except it actually looks more like the Minecraft Prismarine shard, \
-<s>which was definitely not what I was trying to improve in any way, shape or form</s>""",
+>~which was definitely not what I was trying to improve in any way, shape or form~<""",
           "WHO EVEN ARE YOU?":"""\
 Absolutely nobody. I don't exist and I never made anything.
 I'm an enthusiast, a programmer, and also non existent.
@@ -27,12 +27,25 @@ I'm confused and don't know what to do, but this seems cool, so I'll continue do
 Please keep in mind that I have no idea what I'm actually doing, so bugs will occur.""",
           "WHY IS THIS SITE LAGGY?":"""\
 Actually, it's because I don't know how to web design. I just asked for opinions, and you can always submit \
-your feedback over at <a href="https://github.com/VoxelPrismatic/prizm.xyz/issues/new">this link</a> ;]"""}
+your feedback over at >$https://github.com/VoxelPrismatic/prizm.xyz/issues/new$$this link$< ;]"""}
 
 element = document.getElementById("about")
 rep = {'\n':'<br>',
-       '<code>':'<span class="mono dark">',
-       '</code>':'</span>'}
+       '>!':'<span class="mono dark">',
+       '!<': '</span>',
+       '>#': '<b>',
+       '#<': '</b>',
+       '>*': '<i>',
+       '*<': '</i>',
+       '>_': '<u>',
+       '_<': '</u>',
+       '>`': '<div class="mono dark horz" style="width: 95%;">',
+       '`<': '</div>',
+       '>$': '<a href="',
+       '$$': '">',
+       '$<': '</a>',
+       '>~': '<s>',
+       '~<': '</s>'}
 for key in content:
     title = document.createElement("DIV")
     text = document.createTextNode(key)
