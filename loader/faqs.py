6c399bc3d36fca49bbfa6287ff7c14500d468174
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-06 16:05:33 -0500

    Update faqs.py

--- loader/faqs.py
+++ loader/faqs.py
@@ -1,26 +1,8 @@
 from browser import document as doc, html
-
+from priz_md import mark
 content = {"OwO WHAT IS THIS?": "This is an empty FAQ because nobody asked any questions yet UwU"}
 
 element = doc["faq"]
-rep = {'\n':'<br>',
-       '>!':'<span class="mono dark">',
-       '!<': '</span>',
-       '>#': '<b>',
-       '#<': '</b>',
-       '>*': '<i>',
-       '*<': '</i>',
-       '>_': '<u>',
-       '_<': '</u>',
-       '>`': '<div class="mono dark horz" style="width: 95%;">',
-       '`<': '</div>',
-       '>$': '<a href="',
-       '$$': '">',
-       '$<': '</a>',
-       '>~': '<s>',
-       '~<': '</s>'}
 for key in content:
-    for re in rep:
-        content[key] = content[key].replace(re,rep[re])
     element <= html.DIV(key, Class="header")
-    element <= html.DIV(content[key], Class="content consect")
+    element <= html.DIV(mark(content[key]), Class="content consect")
