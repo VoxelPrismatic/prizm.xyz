0739d54d3eb248a6746763277223d0d8839ff50e
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-10 09:59:47 -0500

    Update console.js

--- loader/console.js
+++ loader/console.js
@@ -1,6 +1,14 @@
-function gID(elem) {}
-function gHTML(elem) {}
+function gID(elem) {
+    return document.getElementById(elem);
+}
+function gHTML(elem) {
+    return gID(elem).innerHTML;
+}
 function gSTYLE(elem, name) {}
-function gCLASS(elem, name) {}
-function gEDIT(elem, val) {}
+function gCLASS(elem, name) {
+    gID(elem).classList.toggle(name);
+}
+function gEDIT(elem, val) {
+    gID(elem).innerHTML = val;
+}
 function gST_EDIT(elem, name, val) {}
