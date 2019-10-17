611e7ecd4e284339e29864a521172b2c5b6d240d
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-17 13:41:16 -0500

    Update md_3.py

--- md_3.py
+++ md_3.py
@@ -19,8 +19,8 @@ rep = {
     " ": "\u200b ", #Infinite Spaces
     "§": "\u200b",  #Short NBSP char
     
-    r"(.*)\n(=*)": r"<span class='head1'>\1</span>", #Header 1
-    r"(.*)\n(-*)": r"<span class='head2'>\1</span>", #Header 2
+    r"(.+?)\n(=+?)": r"<span class='head1'>\1</span>", #Header 1
+    r"(.+?)\n(-+?)": r"<span class='head2'>\1</span>", #Header 2
     r"\&(.+)": r"<span class='head1'>\1</span>", #Header 1
     r"\&\&(.+)": r"<span class='head2'>\1</span>", #Header 2
     r"\&\&\&(.+)": r"<span class='head3'>\1</span>", #Header 3
@@ -32,26 +32,26 @@ rep = {
     r"[^\\]\*(.*?)\*": r"<i>\1</i>", #Ital
     r"[^\\]\~(.*?)\~": r"<s>\1</s>", #Strike
     r"[^\\]\_(.*?)\_": r"<u>\1</u>", #Under
+    
     r"[^\\]\+\+(.*?)\+\+": r"<sup>\1</sup>", #Super
     r"[^\\]\-\-(.*?)\-\-": r"<sub>\1</sub>", #Sub
     r"[^\\]\:\:(.*?)\:\:": r"<span class='oL'>\1</sub>", #Overline
     r"[^\\]\|(.*)\|": r"<span class='spoil'>\1</span>", #Spoiler
     
     r"[^\\]\"\"\"((.*|\n)?)\"\"\"": r"<span class='quoted'>\1</span>", #Quoted
-    r"[^\\]\:\:\:[(\w+)\]((.|\n)*?)\:\:\:": r"<span class='note_\1'>\2</span>", #Highlight
+    r"[^\\]\:\:\:[(\w+)]((.|\n)*?)\:\:\:": r"<span class='note_\1'>\2</span>", #Highlight
+    
     r"[^\\]\;\;\;((.|\n)*)\;\;\;": r"<div class='mono dark' style='width: 90%;'>\1</div>", #Code block
     r"[^\\]\`(.*)\`": r"<span class='mono dark'>\1</span>", #Inline code block
     
-    r"[^\\]( *)(\d+)([.)\]}-:;]) (.*)": r"\1\2] \3", #Ordered List
+    r"[^\\]( *)(\d+)([.\)\]\}\-:;]) (.*)": r"\1\2] \3", #Ordered List
     r"[^\\]( *)([-\]>}.~+=]) (.*)": r"\1> \2", #Unordered list
-    
     r"[^\\]\[(.*)]\<(.*)\>": r"<a href='\2'>\1</a>", #Link
-    r"[^\\]\<(.*)\>": r"<a href='\1'>\1</a>", #Link
+    r"[^\\]\<\<(.*)\>\>": r"<a href='\1'>\1</a>", #Link
     r"[^\\]\[\#\[(.*)]\<(.*)\>": r"<embed href='\2' alt='\1'>", #Embed
     
     "\n---\n": "<div class='mdline'>---</div>", #Seperator
     "\n": "<br>", #New Line
-    
     
     #[^\\] - Ignore backslashes
     #(.*) -- Actual content
