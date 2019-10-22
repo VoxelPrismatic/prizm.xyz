391ae96bba69f50951fd9632133e2b69e027c891
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-22 11:55:50 -0500

    Update statuspage.py

--- loader/statuspage.py
+++ loader/statuspage.py
@@ -1,11 +1,10 @@
 from priz_md import mark
 from browser import document as doc, window as win, html
-
 online = False
-current = "There is a `NameError` in the main file"
 when = "6PM CST"
 history = {
-    "10x21_2019": "There was a `NameError` issue in the main file",
+    "10x22_2019": "I'm not too sure what happened, possibly a `SyntaxError` due to a fixed reload function",
+    "10x21_2019": "There was a `NameError` issue in the main file - Resolved Issue",
     "10x17_2019": "PC was off [how?] - Resolved issue",
     "00x00_0000": "Thats the beginning of the history"
 }
@@ -14,4 +13,4 @@ for key in history:
     doc["status"] <= html.DIV(key+" - "+history[key], Id="status_"+key, Class="content consect")
 
 doc["current_status"].innerHTML = "PRIZM IS " + ("ONLINE ;]" if online else "OFFLINE ;[")
-doc["issue"].innerHTML = "No issues here" if online else current+"<br>Will be fixed at "+when
+doc["issue"].innerHTML = "No issues here" if online else mark(history[list(history)[0]]) + "<br>Will be fixed at " + when
