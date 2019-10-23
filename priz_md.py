be2a4ca8d1315d0f1ba61fc932d7e6f3cb605585
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-23 15:38:26 -0500

    Update priz_md.py

--- priz_md.py
+++ priz_md.py
@@ -26,17 +26,6 @@ def mark(st):
     st = st.replace(" ", "\u200b \u200b")
     st = st.replace("§", "\u200b")
     
-    ##/// HEADERS
-    if "&" in st:
-        st = sub(r"\&{1}(.+)", r"<div class='head1'>\1</div>", st) # &Header 1
-        st = sub(r"\&{2}(.+)", r"<div class='head2'>\1</div>", st) # &&Header 2
-        st = sub(r"\&{3}(.+)", r"<div class='head3'>\1</div>", st) # &&&Header 3
-        st = sub(r"\&{4}(.+)", r"<div class='head4'>\1</div>", st) # &&&&Header 4
-        st = sub(r"\&{5}(.+)", r"<div class='head5'>\1</div>", st) # &&&&&Header 5
-        st = sub(r"\&{6}(.+)", r"<div class='head6'>\1</div>", st) # &&&&&&Header 6
-    st = sub(r"(.+?)\n={3,}", r"<div class='head1'>\1</div>", st) # header1 ↵ ===
-    st = sub(r"(.+?)\n-{3,}", r"<div class='head2'>\1</div>", st) # header2 ↵ ---
-    
     ##/// LINKS
     if "<" in st:
         st = sub(r"[^\\]\[(.*)\]\<(.*)\>", r"<a href='\2'>\1</a>", st) # [name]<link>
@@ -44,28 +33,38 @@ def mark(st):
         st = sub(r"[^\\]\#\[(.*)\]\<(.*)\>", r"<embed href='\2' alt='\1'/>", st) # #[alt text]<link>
     
     ##/// BASICS
-    st = sub(r"[^\\]\#(.*?)\#", r"<b>\1</b>", st) # #bolded#
-    st = sub(r"[^\\]\*(.*?)\*", r"<i>\1</i>", st) # *italics*
-    st = sub(r"[^\\]\~(.*?)\~", r"<s>\1</s>", st) # ~strikethrough~
-    st = sub(r"[^\\]\_(.*?)\_", r"<u>\1</u>", st) # _underline_
-    st = sub(r"[^\\]\>\+{2}(.*?)\+{2}\<", r"<sup>\1</sup>", st) # >++superscript++<
-    st = sub(r"[^\\]\>\-{2}(.*?)\-{2}\<", r"<sub>\1</sub>", st) # >--subscript--<
-    st = sub(r"[^\\]\:{2}(.*?)\:{2}", r"<span class='oL'>\1</span>", st) # ::overline::
-    st = sub(r"[^\\]\%(.*?)\%", r"<span class='spoil'>\1</span>", st) # %spoiler%
+    st = sub(r"[^\\]\#(.+?)\#", r"<b>\1</b>", st) # #bolded#
+    st = sub(r"[^\\]\*(.+?)\*", r"<i>\1</i>", st) # *italics*
+    st = sub(r"[^\\]\~(.+?)\~", r"<s>\1</s>", st) # ~strikethrough~
+    st = sub(r"[^\\]\_(.+?)\_", r"<u>\1</u>", st) # _underline_
+    st = sub(r"[^\\]\>\+{2}(.+?)\+{2}\<", r"<sup>\1</sup>", st) # >++superscript++<
+    st = sub(r"[^\\]\>\-{2}(.+?)\-{2}\<", r"<sub>\1</sub>", st) # >--subscript--<
+    st = sub(r"[^\\]\:{2}(.+?)\:{2}", r"<span class='oL'>\1</span>", st) # ::overline::
+    st = sub(r"[^\\]\%(.+?)\%", r"<span class='spoil'>\1</span>", st) # %spoiler%
     
     ##/// ADVANCED
-    st = sub(r'[^\\]\"{3}\n?((.*|\n)?)\n?\"{3}', r"<span class='quoted'>\1</span>", st) # """↵quote block↵"""
-    st = sub(r"[^\\]\!{3}\n?\[(\w+)\] *((.|\n)*?)\n?\!{3}", r"<span class='note_\1'>\2</span>", st) 
+    st = sub(r'[^\\]\"{3}\n?((.+|\n)?)\n?\"{3}', r"<span class='quoted'>\1</span>", st) # """↵quote block↵"""
+    st = sub(r"[^\\]\!{3}\n?\[(\w+)\] *((.|\n)+?)\n?\!{3}", r"<span class='note_\1'>\2</span>", st) 
          # !!![color] notice me senpai!!!
-    st = sub(r"[^\\]\;{3}\n?((.|\n)*)\n?\;{3}", r"<div class='mono dark' style='width: 90%;'>\1</div>", st)
+    st = sub(r"[^\\]\;{3}\n?((.|\n)+)\n?\;{3}", r"<div class='mono dark' style='width: 90%;'>\1</div>", st)
          # ;;;code block;;;
-    st = sub(r"[^\\]\`(.*?)\`", r"<span class='mono dark'>\1</span>", st) # `inline code`
+    st = sub(r"[^\\]\`(.+?)\`", r"<span class='mono dark'>\1</span>", st) # `inline code`
     
     ##/// ORGANIZE
     st = sub(r"\n[^\\]( *)(\d+)([.\)\]\}\-:;])* (.*)", r"\1\2] \3", st) # 1] Ordered list
     st = sub(r"\n[^\\]( *)([-\]>}.~+=])* (.*)", r"\1> \2", st) # > Unordered list
+    st = st.replace("\n-~-\n", "<div class='mdline'>---</div>") # ↵---↵ sep
     
-    st = st.replace("\n---\n", "<div class='mdline'>---</div>") # ↵---↵ sep
+    ##/// HEADERS
+    if "&" in st:
+        st = sub(r"\&{1}(.+)", r"<div class='head1'>##//// \1</div>", st) # &Header 1
+        st = sub(r"\&{2}(.+)", r"<div class='head2'>##/// \1</div>", st) # &&Header 2
+        st = sub(r"\&{3}(.+)", r"<div class='head3'>##/// \1</div>", st) # &&&Header 3
+        st = sub(r"\&{4}(.+)", r"<div class='head4'>#// \1</div>", st) # &&&&Header 4
+        st = sub(r"\&{5}(.+)", r"<div class='head5'>#// \1</div>", st) # &&&&&Header 5
+        st = sub(r"\&{6}(.+)", r"<div class='head6'>#/ \1</div>", st) # &&&&&&Header 6
+    st = sub(r"(.+?)\n={3,}", r"<div class='head1'>##//// \1</div>", st) # header1 ↵ ===
+    st = sub(r"(.+?)\n-{3,}", r"<div class='head2'>##/// \1</div>", st) # header2 ↵ ---
     
     ##/// OTHER
     st = st.replace("\n", "<br>") # newline
@@ -75,6 +74,5 @@ def mark(st):
     #[^\\] - Ignore backslashes
     #\n? --- Ignore newlines
     #\X{n} - Atleast n characters of X
-    #(.*) -- Actual content
-    #(.*?) - Lazy actual content
+    #(.+) -- Actual content
     #(.+?) - Lazy actual content
