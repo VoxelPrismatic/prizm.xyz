be5011af36aa9d9ff82f42f5f26d527a85f3173f
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-22 19:29:29 -0500

    Update statuspage.py

--- loader/statuspage.py
+++ loader/statuspage.py
@@ -3,7 +3,8 @@ from browser import document as doc, window as win, html
 online = False
 when = "6PM CST"
 history = {
-    "10x22_2019": "I'm not too sure what happened, possibly a `SyntaxError` due to a fixed reload function",
+    "10x22_2019": """Issue Found ] The `embedify` module has a `SyntaxError` and is being worked on.
+I'm not too sure what happened, possibly a `SyntaxError` due to a fixed reload function""",
     "10x21_2019": "There was a `NameError` issue in the main file - Resolved Issue",
     "10x17_2019": "PC was off [how?] - Resolved issue",
     "00x00_0000": "Thats the beginning of the history"
