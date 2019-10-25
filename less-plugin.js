82047223bed2fd6f5712cb2c83b3e7e9993262c7
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-25 09:36:04 -0500

    Update less-plugin.js

--- less-plugin.js
+++ less-plugin.js
@@ -1,64 +1,84 @@
 registerPlugin({
     install: function(less, pluginManager, functions) {
+        functions.add('width', function() {
+            w = window.innerWidth;
+            if (            w <= 160 ) {return "90vw";} // BRUH
+            if ( w > 160 && w <= 240 ) {return "90vw";} // M580
+            if ( w > 240 && w <= 360 ) {return "90vw";} // SMOL
+            if ( w > 360 && w <= 640 ) {return "80vw";} // PHONE
+            if ( w > 640 && w <= 720 ) {return "60vw";} // TABLET
+            if ( w > 720 && w <= 1080) {return "50vw";} // PC
+            if (w > 1080 && w <= 1440) {return "40vw";} // LARGE
+            if (w > 1440 && w <= 2160) {return "40vw";} // 2K
+            if (w > 2160 && w <= 3240) {return "40vw";} // 3K
+            if (w > 3240 && w <= 4320) {return "40vw";} // 4K
+            if (w > 4320 && w <= 5480) {return "40vw";} // 5K
+            if (w > 5480 && w <= 8640) {return "40vw";} // 8K, and wow that is big
+            return "30vw";
+        });
         functions.add('fonts_code', function() {
             w = window.innerWidth;
             if (            w <= 160 ) {return "3.2vw";} // BRUH
             if ( w > 160 && w <= 240 ) {return "3.2vw";} // M580
             if ( w > 240 && w <= 360 ) {return "3.2vw";} // SMOL
             if ( w > 360 && w <= 640 ) {return "3.2vw";} // PHONE
-            if ( w > 640 && w <= 720 ) {return "3.2vw";} // TABLET
-            if ( w > 720 && w <= 1080) {return "3.2vw";} // PC
-            if (w > 1080 && w <= 1440) {return "3.2vw";} // LARGE
-            if (w > 1440 && w <= 2160) {return "3.2vw";} // 2K
-            if (w > 2160 && w <= 3240) {return "3.2vw";} // 3K
-            if (w > 3240 && w <= 4320) {return "3.2vw";} // 4K
-            if (w > 4320 && w <= 5480) {return "3.2vw";} // 5K
-            if (w > 5480 && w <= 8640) {return "3.2vw";} // 8K, and wow that is big
+            if ( w > 640 && w <= 720 ) {return "1.5vw";} // TABLET
+            if ( w > 720 && w <= 1080) {return "1.5vw";} // PC
+            if (w > 1080 && w <= 1440) {return "1.5vw";} // LARGE
+            if (w > 1440 && w <= 2160) {return "1.0vw";} // 2K
+            if (w > 2160 && w <= 3240) {return "1.0vw";} // 3K
+            if (w > 3240 && w <= 4320) {return "1.0vw";} // 4K
+            if (w > 4320 && w <= 5480) {return "1.0vw";} // 5K
+            if (w > 5480 && w <= 8640) {return "1.0vw";} // 8K, and wow that is big
+            return "0.5vw";
         });
         functions.add('fonts_head', function() {
             w = window.innerWidth;
-            if (            w <= 160 ) {return "3.2vw";} // BRUH
-            if ( w > 160 && w <= 240 ) {return "3.2vw";} // M580
-            if ( w > 240 && w <= 360 ) {return "3.2vw";} // SMOL
-            if ( w > 360 && w <= 640 ) {return "3.2vw";} // PHONE
-            if ( w > 640 && w <= 720 ) {return "3.2vw";} // TABLET
-            if ( w > 720 && w <= 1080) {return "3.2vw";} // PC
-            if (w > 1080 && w <= 1440) {return "3.2vw";} // LARGE
-            if (w > 1440 && w <= 2160) {return "3.2vw";} // 2K
-            if (w > 2160 && w <= 3240) {return "3.2vw";} // 3K
-            if (w > 3240 && w <= 4320) {return "3.2vw";} // 4K
-            if (w > 4320 && w <= 5480) {return "3.2vw";} // 5K
-            if (w > 5480 && w <= 8640) {return "3.2vw";} // 8K, and wow that is big
+            if (            w <= 160 ) {return "4.0vw";} // BRUH
+            if ( w > 160 && w <= 240 ) {return "4.0vw";} // M580
+            if ( w > 240 && w <= 360 ) {return "4.0vw";} // SMOL
+            if ( w > 360 && w <= 640 ) {return "3.0vw";} // PHONE
+            if ( w > 640 && w <= 720 ) {return "1.5vw";} // TABLET
+            if ( w > 720 && w <= 1080) {return "1.2vw";} // PC
+            if (w > 1080 && w <= 1440) {return "1.0vw";} // LARGE
+            if (w > 1440 && w <= 2160) {return "1.0vw";} // 2K
+            if (w > 2160 && w <= 3240) {return "1.0vw";} // 3K
+            if (w > 3240 && w <= 4320) {return "1.0vw";} // 4K
+            if (w > 4320 && w <= 5480) {return "1.0vw";} // 5K
+            if (w > 5480 && w <= 8640) {return "1.0vw";} // 8K, and wow that is big
+            return "1.0vw";
         });
         functions.add('fonts_cont', function() {
             w = window.innerWidth;
-            if (            w <= 160 ) {return "3.2vw";} // BRUH
-            if ( w > 160 && w <= 240 ) {return "3.2vw";} // M580
-            if ( w > 240 && w <= 360 ) {return "3.2vw";} // SMOL
-            if ( w > 360 && w <= 640 ) {return "3.2vw";} // PHONE
-            if ( w > 640 && w <= 720 ) {return "3.2vw";} // TABLET
-            if ( w > 720 && w <= 1080) {return "3.2vw";} // PC
-            if (w > 1080 && w <= 1440) {return "3.2vw";} // LARGE
-            if (w > 1440 && w <= 2160) {return "3.2vw";} // 2K
-            if (w > 2160 && w <= 3240) {return "3.2vw";} // 3K
-            if (w > 3240 && w <= 4320) {return "3.2vw";} // 4K
-            if (w > 4320 && w <= 5480) {return "3.2vw";} // 5K
-            if (w > 5480 && w <= 8640) {return "3.2vw";} // 8K, and wow that is big
+            if (            w <= 160 ) {return "3.5vw";} // BRUH
+            if ( w > 160 && w <= 240 ) {return "3.5vw";} // M580
+            if ( w > 240 && w <= 360 ) {return "3.5vw";} // SMOL
+            if ( w > 360 && w <= 640 ) {return "2.5vw";} // PHONE
+            if ( w > 640 && w <= 720 ) {return "1.3vw";} // TABLET
+            if ( w > 720 && w <= 1080) {return "1.0vw";} // PC
+            if (w > 1080 && w <= 1440) {return "0.8vw";} // LARGE
+            if (w > 1440 && w <= 2160) {return "0.5vw";} // 2K
+            if (w > 2160 && w <= 3240) {return "0.5vw";} // 3K
+            if (w > 3240 && w <= 4320) {return "0.5vw";} // 4K
+            if (w > 4320 && w <= 5480) {return "0.5vw";} // 5K
+            if (w > 5480 && w <= 8640) {return "0.5vw";} // 8K, and wow that is big
+            return "0.5vw";
         });
         functions.add('fonts_warn', function() {
             w = window.innerWidth;
-            if (            w <= 160 ) {return "3.2vw";} // BRUH
-            if ( w > 160 && w <= 240 ) {return "3.2vw";} // M580
-            if ( w > 240 && w <= 360 ) {return "3.2vw";} // SMOL
-            if ( w > 360 && w <= 640 ) {return "3.2vw";} // PHONE
-            if ( w > 640 && w <= 720 ) {return "3.2vw";} // TABLET
-            if ( w > 720 && w <= 1080) {return "3.2vw";} // PC
-            if (w > 1080 && w <= 1440) {return "3.2vw";} // LARGE
-            if (w > 1440 && w <= 2160) {return "3.2vw";} // 2K
-            if (w > 2160 && w <= 3240) {return "3.2vw";} // 3K
-            if (w > 3240 && w <= 4320) {return "3.2vw";} // 4K
-            if (w > 4320 && w <= 5480) {return "3.2vw";} // 5K
-            if (w > 5480 && w <= 8640) {return "3.2vw";} // 8K, and wow that is big
+            if (            w <= 160 ) {return "3.0vw";} // BRUH
+            if ( w > 160 && w <= 240 ) {return "3.0vw";} // M580
+            if ( w > 240 && w <= 360 ) {return "2.0vw";} // SMOL
+            if ( w > 360 && w <= 640 ) {return "1.5vw";} // PHONE
+            if ( w > 640 && w <= 720 ) {return "1.5vw";} // TABLET
+            if ( w > 720 && w <= 1080) {return "1.5vw";} // PC
+            if (w > 1080 && w <= 1440) {return "1.0vw";} // LARGE
+            if (w > 1440 && w <= 2160) {return "1.0vw";} // 2K
+            if (w > 2160 && w <= 3240) {return "1.0vw";} // 3K
+            if (w > 3240 && w <= 4320) {return "1.0vw";} // 4K
+            if (w > 4320 && w <= 5480) {return "1.0vw";} // 5K
+            if (w > 5480 && w <= 8640) {return "1.0vw";} // 8K, and wow that is big
+            return "1.0vw";
         });
         functions.add('fonts_foot', function() {
             w = window.innerWidth;
