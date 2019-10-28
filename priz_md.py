601b1e02b5ab451453eda07af001e618c8db3503
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-28 17:32:46 -0500

    Update priz_md.py

--- priz_md.py
+++ priz_md.py
@@ -23,10 +23,10 @@ def link(st):
     st = " "+st
     ##/// LINKS
     if "<" in st:
-        st = sub(r"[^\\]\#\{CS (.*)\|(.*)\|(.*)\}\<(.*)\>",
+        st = sub(r"[^\\]\#\{CS ([\w\d ]+)\|([\w\d ]+)\|(.*)\}\<(.*)\>",
                  r"<a href='\4'><button class='linky \1' style='\2'>\3</button></a>", st)
-        st = sub(r"[^\\]\#\{S (.*)\|(.*)\}\<(.*)\>", r"<a href='\3'><button class='linky' style='\1'>\2</button><\/a>", st)
-        st = sub(r"[^\\]\#\{C (.*)\|(.*)\}\<(.*)\>", r"<a href='\3'><button class='linky \1'>\2</button></a>", st)
+        st = sub(r"[^\\]\#\{S ([\w\d ]+)\|(.*)\}\<(.*)\>", r"<a href='\3'><button class='linky' style='\1'>\2</button><\/a>", st)
+        st = sub(r"[^\\]\#\{C ([\w\d ]+)\|(.*)\}\<(.*)\>", r"<a href='\3'><button class='linky \1'>\2</button></a>", st)
         st = sub(r"[^\\]\#\{(.*)\}\<(.*)\>", r"<a href='\2'><button class='linky'></button></a>", st)
         st = sub(r"[^\\]\#\[(.*)\]\<(.*)\>", r"<embed src='\2' alt='\1'/>", st) # #[alt text]<link> 
         st = sub(r"[^\\]\[(.*)\]\<(.*)\>", r"<a href='\2'>\1</a>", st) # [name]<link>
