7aec0dd50413f7e127c44097132f6499ad119fbd
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-28 16:08:11 -0500

    Update markup.py

--- loader/markup.py
+++ loader/markup.py
@@ -11,28 +11,35 @@ for el in ls:
 
 ls = doc.select(".markelem")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = elem(el.innerHTML)
 
 ls = doc.select(".marklink")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = link(el.innerHTML)
     
 ls = doc.select(".markbasic")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = basic(el.innerHTML)
 
 ls = doc.select(".markadvan")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = advan(el.innerHTML)
     
 ls = doc.select(".marksetup")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = setup(el.innerHTML)
     
 ls = doc.select(".markhead")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = head(el.innerHTML)
     
 ls = doc.select(".markother")
 for el in ls:
+    el.innerHTML = init(el.innerHTML)
     el.innerHTML = other(el.innerHTML)
