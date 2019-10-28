1b29eb04c7b5295bc85cd3c6f1f1e279bfe65ab3
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-28 16:04:51 -0500

    Update priz_md.py

--- priz_md.py
+++ priz_md.py
@@ -94,6 +94,7 @@ def elem(st):
     st = " "+st
     st = sub(r"\[\[\{(.*)\}(.*)\]\]", r'<button class="linky" style="\1">\2<\/button">', st) # [[{style}button]]
     st = sub(r"\{TAG (\w+) (.+?)\}", r"<\1>\2<\/\1>", st) # {TAG div content} --> <div>content</div>
+    return st[1:]
     
 def mark(st):
     st = init(st)
