8c3ce495d426205c967507e93f380628dd8e175b
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-29 10:33:46 -0500

    Update statuspage.py

--- loader/statuspage.py
+++ loader/statuspage.py
@@ -1,8 +1,9 @@
 from priz_md import mark
 from browser import document as doc, window as win, html
-online = True
-when = "6PM CST"
+online = None
+when = "7PM CST"
 history = {
+    "10x29_2019": "RAM usage is a bit high, so slow response times may occur"
     "10x22_2019": "The `embedify` module has a `SyntaxError` - Resolved issue",
     "10x21_2019": "There was a `NameError` issue in the main file - Resolved Issue",
     "10x17_2019": "PC was off [how?] - Resolved issue",
@@ -12,5 +13,5 @@ for key in history:
     history[key] = mark(history[key])
     doc["status"] <= html.DIV(key+" - "+history[key], Id="status_"+key, Class="content consect")
 
-doc["current_status"].innerHTML = "PRIZM IS " + ("ONLINE ;]" if online else "OFFLINE ;[")
+doc["current_status"].innerHTML = "PRIZM IS " + ("ONLINE ;]" if online in [True, None] else "OFFLINE ;[")
 doc["issue"].innerHTML = "No issues here" if online else mark(history[list(history)[0]]) + "<br>Will be fixed at " + when
