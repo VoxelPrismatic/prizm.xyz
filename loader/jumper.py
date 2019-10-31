cdc2346af8d16074ee5dab639f0c4182c9fb3508
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-10-31 09:42:36 -0500

    Update jumper.py

--- loader/jumper.py
+++ loader/jumper.py
@@ -5,12 +5,29 @@ if "#" in url:
     if area.startswith("changes_"):
         doc["btn_change"].click()
         doc["changes"].scrollIntoView()
-        elem = doc[area]
+        elem = doc[area.split("_")[-1]]
         if elem: elem.click()
+            
     elif area.startswith("about_"):
         doc["btn_about"].click()
         doc["about"].scrollIntoView()
-        elem = doc[area]
+        elem = doc[area.split("_")[-1]]
         if elem: elem.scrollIntoView()
-    elif area.startswith("
+            
+    elif area.startswith("faq_"):
+        doc["btn_faq"].click()
+        doc["faq"].scrollIntoView()
+        elem = doc[area.split("_")[-1]]
+        if elem: elem.scrollIntoView()
+        
+    elif area.startswith("status_"):
+        doc["btn_status"].click()
+        doc["status"].scrollIntoView()
+        elem = doc[area.split("_")[-1]]
+        if elem: elem.scrollIntoView()
+    
+    elif area.startswith("commands_"):
+        doc["changes"].scrollIntoView()
+        elem = doc[area.split("_")[-1]]
+        if elem: elem.click()
         
