b6a70fe9199cfdb3260ca5fb002a32247bd77e4b
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-20 08:37:12 -0600

    Update statuspage.py

--- loader/statuspage.py
+++ loader/statuspage.py
@@ -1,11 +1,15 @@
 from priz_md import mark
 from browser import document as doc, window as win, html
+import datetime as dt
+
 online = False
-when = "an unknown time"
+when = "4:30PM CST"
+
 history = {
-    "11x06_2019": "HDD died... waiting for a good SSD price to show up",
-    "10x31_2019": "There is a connection issue on my end - Fixed",
-    "10x29_2019": "RAM usage is a bit high, so slow response times or even crashes may occur - resolved",
+    "11x20_2019": "Linux power settings are not set up"
+    "11x06_2019": "HDD died... waiting for a good SSD price to show up - Resolved issue",
+    "10x31_2019": "There is a connection issue on my end - Resolved issue",
+    "10x29_2019": "RAM usage is a bit high, so slow response times or even crashes may occur - Resolved issue",
     "10x22_2019": "The `embedify` module has a `SyntaxError` - Resolved issue",
     "10x21_2019": "There was a `NameError` issue in the main file - Resolved Issue",
     "10x17_2019": "PC was off [how?] - Resolved issue",
