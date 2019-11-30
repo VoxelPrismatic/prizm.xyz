460a8eeb1e60ee715ae84dc514a09dbb088ed160
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-30 13:47:38 -0600

    Create projects.py

---
+++ loader/projects.py
@@ -0,0 +1,24 @@
+from browser import document as doc, html, window as win
+from priz_md import mark
+content = eval(open("json/projects.json").read())
+
+element = doc["projs"]
+
+for key in content:
+    element <= html.DIV(
+        mark(content[key]["nam"])+"<br>", 
+        Class = "blue", 
+        Id = "projects_"+key["nam"],
+    )
+    element <= html.DIV(
+        mark(content[key]["dsc"])+"<br>", 
+        Class = "lgrey", 
+    )
+    element <= html.DIV(
+        mark(content[key]["oth"])+"<br>",
+        Class = "dgry", 
+    )
+    element <= html.DIV(
+        mark(content[key]["lnk"])+"<br>",
+        style = {"color": "#ff0088 !important"}
+    )
